4555d1ea0e675a0f84c73f5d77ff5ba45522eb43
cyanray <a484599279@hotmail.com>
2021-11-20 13:56:26 +0800

    init.

--- ZhuZhuBot/Program.cs
+++ ZhuZhuBot/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace ZhuZhuBot
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+}
